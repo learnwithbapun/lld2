0f3ba51ce52dfcd48ff5c053b2a80f496004dc36
Bapun <learnwithbapun@gmail.com>
2024-02-23 01:19:28 +0530

    Added

---
+++ src/main/java/SbiBank.java
@@ -0,0 +1,31 @@
+import polymorphism.Bank;
+
+public final class SbiBank extends Bank {
+	
+	
+
+	int interestRate;
+	
+	public int interestRate(int id) {
+
+		return 1;
+	}
+
+	public int interestRate(int id, int creditScrore) {
+
+		return 1;
+	}
+
+	public int interestRate(int id, int creditScrore, int age) {
+
+		return 1;
+	}
+
+	
+	public static void main(String[] args) {
+		
+		
+		Bank bank = new SbiBank();
+		bank.interestRate(2);
+	}
+}
---
+++ src/main/java/polymorphism/Bank.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+public class Bank {
+
+	public int interestRate(int id) {
+		return 1;
+	}
+
+	public int interestRate(int id, int creditScrore) {
+		return 1;
+	}
+
+	public int interestRate(int id, int creditScrore, int age) {
+		return 1;
+	}
+
+}
